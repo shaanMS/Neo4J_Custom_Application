0f972213bdaef050c9148948ed0483be48685615
shaanMS <96082415+shaanMS@users.noreply.github.com>
2023-05-31 23:11:39 +0530

    dependencies Added

---
+++ Neo4J_1/src/main/java/com/mycompany/neo4j_1/Neo4J_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.neo4j_1;
+
+/**
+ *
+ * @author bhayu
+ */
+public class Neo4J_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
